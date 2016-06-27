780d594e392a133ed7236d67f3d5965da3e92db0
Vivek Maharajh <vmaha@ntdev.corp.microsoft.com>
2016-06-26 21:39:40 -0700

    got demo working

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,4 +1,41 @@
-indexApp.controller('pageHome', ['$scope',
-    function($scope) {
-    }
-]);
+indexApp.controller(
+    'pageHome', 
+    [
+        '$scope',
+        'clientRectNotifications',
+        function($scope, clientRectNotifications) {
+            var trackerElement = document.getElementsByClassName('tracker')[0];
+
+            $scope.notifications = [];
+            function OnNotification(type) {
+                $scope.notifications.unshift(type);
+                if ($scope.notifications.length > 20) {
+                    $scope.notifications.pop();
+                }
+                $scope.$apply();
+            }
+
+            clientRectNotifications.register(
+                trackerElement,
+                {
+                    completelyOutOfView : function() {
+                        OnNotification('completelyOutOfView');
+                        console.log('completelyOutOfView');
+                    }, 
+                    completelyInView : function() {
+                        OnNotification('completelyInView');
+                        console.log('completelyInView');
+                    },
+                    mostlyInView : function() {
+                        OnNotification('mostlyInView');
+                        console.log('mostlyInView');
+                    },
+                    partiallyInView : function() {
+                        OnNotification('partiallyInView');
+                        console.log('partiallyInView');
+                    }
+                }
+            );
+        }
+    ]
+);

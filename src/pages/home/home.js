7dae7e62179f7cf036dd1980317373cd6d50ca87
Vivek Maharajh <vmaha@ntdev.corp.microsoft.com>
2016-06-27 13:11:58 -0700

    some code cleanup

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,39 +1,43 @@
-var app = require('../../scripts/indexApp').app;
+(function() {
 
-app.controller(
-    'pageHome', 
-    [
-        '$scope',
-        'ngUiClientRectNotifications',
-        function($scope, ngUiClientRectNotifications) {
-            var trackerElement = document.getElementsByClassName('tracker')[0];
+    var app = require('../../scripts/indexApp').app;
+    app.controller(
+        'pageHome', 
+        [
+            '$scope',
+            'ngUiClientRectNotifications',
+            function($scope, ngUiClientRectNotifications) {
+                var trackerElement = document.getElementsByClassName('tracker')[0];
 
-            $scope.notifications = [];
-            function OnNotification(type) {
-                $scope.notifications.unshift(type);
-                if ($scope.notifications.length > 20) {
-                    $scope.notifications.pop();
-                }
-                $scope.$apply();
-            }
-
-            ngUiClientRectNotifications.register(
-                trackerElement,
-                {
-                    completelyOutOfView : function() {
-                        OnNotification('completelyOutOfView');
-                    }, 
-                    completelyInView : function() {
-                        OnNotification('completelyInView');
-                    },
-                    mostlyInView : function() {
-                        OnNotification('mostlyInView');
-                    },
-                    partiallyInView : function() {
-                        OnNotification('partiallyInView');
+                $scope.notifications = [];
+                function OnNotification(type) {
+                    $scope.notifications.unshift(type);
+                    if ($scope.notifications.length > 20) {
+                        $scope.notifications.pop();
                     }
+                    $scope.$apply();
                 }
-            );
-        }
-    ]
-);
+
+                ngUiClientRectNotifications.register(
+                    trackerElement,
+                    {
+                        completelyOutOfView : function() {
+                            OnNotification('completelyOutOfView');
+                        }, 
+                        completelyInView : function() {
+                            OnNotification('completelyInView');
+                        },
+                        mostlyInView : function() {
+                            OnNotification('mostlyInView');
+                        },
+                        partiallyInView : function() {
+                            OnNotification('partiallyInView');
+                        }
+                    }
+                );
+            }
+        ]
+    );
+
+})();
+
